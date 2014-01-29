2dda8c91e57063acac9596a0ae15007e6bf30002
mirkohin1991 <mirko.hin@live.de>
2014-01-29 16:27:47 +0100

    new app

---
+++ Day/src/de/smbsolutions/day/presentation/activities/MainActivity.java
@@ -0,0 +1,78 @@
+package de.smbsolutions.day.presentation.activities;
+
+import de.smbsolutions.day.R;
+import de.smbsolutions.day.functions.database.*;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract.Contacts.Data;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Database.getInstance(this);
+	
+			
+	}
+	public void onButtonClick(View view){
+		switch (view.getId()) {
+		case R.id.button1:
+			startActivity(new Intent(this, UnterActivity.class));
+			break;
+		case R.id.button2:
+			startActivity(new Intent(this,KameraActivity.class));
+		default:
+			break;
+		}
+	}
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		super.onRestart();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+	}
+
+	
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
